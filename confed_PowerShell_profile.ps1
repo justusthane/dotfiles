65a5ff297c12e2d8e1824a61027bbaea3d79af78
jbadergr (Justus Grunow) <jbadergr@confederationcollege.ca>
2021-07-26 14:08:56 -0400

    ConEmu setting changes

--- confed_PowerShell_profile.ps1
+++ confed_PowerShell_profile.ps1
@@ -1,41 +1,6 @@
 remove-item alias:curl
 
-. ~\PowerShellScripts\Load-Scripts.ps1
-
-  
-function Check-LoadedModule {
-  Param( [parameter(Mandatory = $true)][alias("Module")][string]$ModuleName)
-    $LoadedModules = Get-Module | Select Name
-    if (!$LoadedModules -like "*$ModuleName*") {Import-Module -Name $ModuleName}
-}
-
-function ipinfo {
- param (
-   $IPAddress
- )
- curl https://ipinfo.io/$IPAddress
-}
-
-function Get-MigrationBatchStatus {
-  param (
-    $BatchID
-  )
-  Check-LoadedModule "ExchangeOnlineManagement"
-  get-migrationuser -BatchId $BatchID | sort DataConsistencyScore | select Identity,Status,ErrorSummary,DataConsistencyScore,HasUnapprovedSkippedItems
-}
-
-function Get-SkippedItems {
-  param (
-    $BatchID
-  )
-  Check-LoadedModule "ExchangeOnlineManagement"
-  get-migrationuser -BatchId $BatchID | 
-  ?{ $_.HasUnapprovedSkippedItems -eq $True } | 
-  Get-MigrationUserStatistics -IncludeSkippedItems | 
-  select -Expand SkippedItems @{label="UserIdentity";expression={$_.Identity}} | 
-  ? {$_.Kind -ne "CorruptFolderACL" } | 
-  select @{label="Identity";expression={$_.UserIdentity}},Kind,FolderName,Subject,DateReceived,@{label="MessageSizeMB";expression={$_.MessageSize/1024/1024}}
-}
+#. ~\PowerShellScripts\Load-Scripts.ps1
 
 
 if ($host.Name -eq 'ConsoleHost') {
@@ -44,3 +9,5 @@ if ($host.Name -eq 'ConsoleHost') {
   Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
   Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 }
+
+Import-Module PSFzf
