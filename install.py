5154f8f56ad7fa2649d72dda5b605d62a6c70b31
Justus Grunow <jbadergr@confederationcollege.ca>
2024-10-25 16:40:02 -0400

    Script now creates symlink for vimrc if it doesn't already exists (or can overwrite it)

--- install.py
+++ install.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
-from os.path import expanduser
-from os import symlink
+import sys
+import os
 from platform import system
+import argparse
 
-home_dir = expanduser("~")
+parser=argparse.ArgumentParser()
+parser.add_argument("--force", action="store_true", help="Overwrite existing target files if they exist")
+args=parser.parse_args()
+
+home_dir = os.path.expanduser("~")
 if(system() == "Windows"):
     nvim_config_dir = f"{home_dir}/AppData/Local/nvim"
 elif(system() == "Linux"):
@@ -11,4 +16,19 @@ elif(system() == "Linux"):
 else:
     raise Exception("Unknown platform")
 
-symlink(f"{home_dir}/dotfiles/init.vim",f"{nvim_config_dir}/init.vim")
+def create_symlink(link, target):
+    print(f"Symlinking {link} -> {target}...")
+    if os.path.lexists(link) and args.force:
+        print(f"\t{link} already exists. Overwriting it.")
+        os.unlink(link)
+        os.symlink(f"{target}",f"{link}") 
+        print("\tDone")
+    elif os.path.lexists(link):
+        print(f"\t{link} already exists.")
+    else:
+        os.symlink(f"{target}",f"{link}") 
+        print("\tDone")
+
+create_symlink(link = f"{nvim_config_dir}/init.vim", 
+               target = f"{home_dir}/dotfiles/init.vim")
+
